c860239f506a30707f72e047e263897bc275bd49
Cesar Torres <>
2021-04-11 02:15:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+height_float = float(height)
+weight_float = float(weight)
+
+result = int (weight_float / (height_float**2))
+print(result)
+
+
+
+
+
+
+
+
+
